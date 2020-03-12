36a48f56679d5ae26cedfbaa2f5856047c5c166f
RickStorm <39725999+RickStorm@users.noreply.github.com>
2020-03-12 20:32:58 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #Initialize the variable holding the cached inverse matrix
+  cachedInverse <- NULL
+  
+  # sets the value of matrix to y using the superassignment operator 
+  # from other environment in the search() list
+  set <- function(y) {
+    x <<- y
+    cachedInverse <<- NULL
+  }
+  
+  
+  get <- function() x
+  # get the value of x
+  
+  setInverse <- function(inverse) cachedInverse <<- inverse
+  
+  getInverse <- function() cachedInverse
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
@@ -12,4 +33,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+ 
+  invFunc <- x$getInverse()
+  
+  if(!is.null(invFunc)) {
+    message("getting cached data")
+    return(invFunc)
+  }
+  data <- x$get()
+  invFunc <- solve(data, ...)
+  x$setInverse(invFunc)
+  invFunc
 }
